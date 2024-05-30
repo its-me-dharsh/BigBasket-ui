c5e779039683e5f6c46c7ad9269f47ad7c74bd32
its-me-dharsh <dharshinianand2005@gmail.com>
2024-05-30 15:28:16 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import BigBasket from './BigBasket.js';
+function App() {
+  return (
+   <BigBasket></BigBasket>
+  );
+}
+
+export default App;
---
+++ src/BigBasket.js
@@ -0,0 +1,190 @@
+function BigBasket(){
+    return(
+        <div>
+            <div className="header">
+            <h3 style={{color:"red",display : "inline-block",marginLeft:"125px"}}>big</h3>
+            <h3 style={{display : "inline-block"}} className="basket">basket</h3>
+            <input type="textfield" placeholder="🔍  Search for Products..." className="search"></input>
+            <button className="location">Select Location</button>
+            <button className="login">Login/SignUp</button><br></br>
+            <select className ="dropdown"><option>Shop by Category</option></select>
+            <button className="items">Exotic fruits and V...</button>
+            <button className="items">Tea</button>
+            <button className="items">Ghee</button>
+            <button className="items">Sea food</button>
+            <button className="items">Fresh vegetables</button>
+            <img src="https://www.bigbasket.com/media/uploads/banner_images/Smart_basket-250124-1.png" style={{"paddingLeft":"20px"}}></img>
+            <img src="https://www.bigbasket.com/media/uploads/banner_images/hp_cmc_m_offer_28_220921_all.png" style={{"paddingLeft":"20px"}}></img>
+            <br></br><br></br>
+            </div>
+
+            <div className="button-container">
+            <button className="action-button" style={{"marginLeft":"110px"}}>EGGS, MEAT<br></br> AND FISH</button>
+            <button className="action-button neu-pass">NEUPASS</button>
+            <button className="action-button ayurveda">AYURVEDA</button>
+            <button className="action-button">BUY MORE SAVE MORE</button>
+            <button className="action-button">DEALS OF THE WEEK</button>
+            <button className="action-button" style={{"marginRight":"150px"}}>COMBO STORE</button>
+            </div>
+
+          
+            <div className="cart">
+                <br></br>
+                <h2 style={{"marginLeft":"15px"}}>My Smart Basket</h2>
+                <div className="card" >
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/10000067_23-fresho-capsicum-green.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"marginLeft":"13px","color":"rgb(160, 160, 160)"}}>Fresho</p>
+                    <p>Capsicum - Green (Loose)</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>1 kg</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 84  </h3>
+                    <h4 className="strike">Rs 115.07</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <div className="card">
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/10000070_15-fresho-carrot-orange.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"color":"rgb(160, 160, 160)"}}>Fresho</p>
+                    <p>Carrot - Orange (Loose)</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>1 kg</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 80  </h3>
+                    <h4 className="strike">Rs 115.07</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <div className="card">
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/10000338_13-fresho-ginger.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"color":"rgb(160, 160, 160)"}}>Fresho</p>
+                    <p>Ginger (Loose)</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>1 kg</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 230  </h3>
+                    <h4 className="strike">Rs 315.07</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <div className="card">
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/10000326_14-fresho-coriander-leaves.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"color":"rgb(160, 160, 160)"}}>Fresho</p>
+                    <p>Corriander Leaves</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>1 kg</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 340  </h3>
+                    <h4 className="strike">Rs 465.75</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <br></br><br></br><br></br>
+            </div>
+           
+            <br></br><br></br><br></br>
+            <div className="cart">
+                <br></br>
+                <h2 style={{"marginLeft":"15px"}}>Best Sellers</h2>
+                <div className="card" >
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/1224872_1-littles-baby-wipes-with-aloe-vera-jojoba-oil-vitamin-e.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"marginLeft":"13px","color":"rgb(160, 160, 160)"}}>Littles</p>
+                    <p>Baby wipes with alovera, jojoba oil</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>4x80 pieces multipack</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 281  </h3>
+                    <h4 className="strike">Rs 760</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <div className="card">
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/40202338_5-pampers-baby-wipes-with-aloe.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"color":"rgb(160, 160, 160)"}}>Pampers</p>
+                    <p>Baby wipes with aloe</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>72 pieces</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 197  </h3>
+                    <h4 className="strike">Rs 299</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <div className="card">
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/1201455_3-bigbasket-baby-wipes-sea-breeze-no-paraben-double-moisturizing.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"color":"rgb(160, 160, 160)"}}>Bigbasket</p>
+                    <p>Baby wipes sea breeze</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>2x80 pieces multipack</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 210  </h3>
+                    <h4 className="strike">Rs 398</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <div className="card">
+
+                    <img className="image" src="https://www.bigbasket.com/media/uploads/p/m/1226972_1-littles-baby-diaper-pants-xl-with-wetness-indicator-12-hours-absorption.jpg?tr=w-1920,q=80" style={{"width":"250px","margin":"14px"}}></img>
+                    <p style={{"color":"rgb(160, 160, 160)"}}>Littles</p>
+                    <p>Baby diaper pants XL</p>
+                    <br></br>
+                    <br></br>
+                    <select className="quantity-dropdown"><option>2x24 pieces multipack</option></select>
+                    <br></br>
+                    <h3 style={{"display":"inline-block","marginLeft":"11px"}}>Rs 269.96  </h3>
+                    <h4 className="strike">Rs 898</h4>
+                    <br></br><br></br><br></br><br></br><br></br>
+                    <button className="AddButton">Add</button>
+                    <br></br><br></br>
+                </div>
+                <br></br><br></br><br></br>
+            </div>
+            <br></br>
+        <div className="footer">
+        <p><b>bigbasket - online grocery shop</b></p>
+        <p>Did you ever imagine that the freshest of fruits and vegetables, top-quality pulses and food grains, dairy products, and hundreds of branded items could be handpicked and delivered to your home, all at the click of a button? In today's fast-paced world, bigbasket.com, India's pioneering online grocery store, continues to bring a staggering array of over 40,000 products from more than 1,000 brands to the doorsteps of over 10 million satisfied customers. From essential household cleaning products to the latest beauty and makeup trends, bigbasket remains your one-stop shop for daily needs.
+
+        </p>
+        <p>In these times, we've eliminated the stress associated with shopping for daily essentials. You can now effortlessly order all your household products and groceries online. Plus, the added convenience of finding all your requirements at a single source, coupled with substantial savings, demonstrates that bigbasket, India's largest online supermarket, has transformed the way we shop for groceries. Online grocery shopping has become second nature. And when it comes to freshness, whether it's fruits and vegetables or dairy and meat, we've got you covered! Easily obtain fresh eggs, meat, fish, and more with just a few clicks.
+        </p>
+
+        <p>We now serve 300+ cities and towns across India and ensure swift delivery times, guaranteeing that all your groceries, snacks and branded foods reach you on time.
+
+        </p>
+
+        <p>Slotted Delivery: Choose the most convenient delivery slot to receive your groceries, ranging from early morning delivery for early birds to late-night delivery for those on the night shift. bigbasket caters to every schedule.
+
+        </p>
+        <p>Instant delivery from bbnow: In response to the ever-increasing demand for convenience, bbnow by bigbasket offers lightning-fast grocery delivery, ensuring that your essentials are at your doorstep within 15-30 minutes. Our quick delivery service has revolutionized the way you shop for groceries. Choose from 5000+ grocery essentials. bbnow is available only in select cities.
+
+        </p>
+        <p>Whether it's a last-minute dinner party or you simply need something urgently, we've got you covered. This service exemplifies our commitment to providing you with not just the widest range of products but also the fastest and most efficient shopping experience, making bigbasket.com the go-to destination for all your immediate grocery needs.</p>
+        
+        </div>
+        <div className="bottom"></div>
+        </div>
+    );
+}
+
+export default BigBasket;
